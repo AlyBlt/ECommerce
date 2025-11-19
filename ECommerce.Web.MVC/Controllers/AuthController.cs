d03fa66af678ef1a05a59ca4fbd70f206450f6a8
tombiks <tncybaktir@gmail.com>
2025-11-20 02:29:23 +0300

    1.images'i img yaptim iconlarla nasıl görünüyor diye. 2.login-register-forgotpassword viewleri tamamlandı 3.css folder'ina auth-style.css file'i eklendi. views/auth içindeki cshtml'ler auth-style.css'den besleniyor. 4.routelarda düzenleme yapıldı. şuan aktif haliyle layout'da login'e basınca viewler aktif olarak çalışıyor.

--- ECommerce.Web.MVC/Controllers/AuthController.cs
+++ ECommerce.Web.MVC/Controllers/AuthController.cs
@@ -1,25 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNetCoreGeneratedDocument;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerceWeb.MVC.Controllers
 {
     //Aliye Genel Not: Şu an sadece GET ile sayfayı döndürüyor.
     //Ödevin bu kısmında form gönderimi yapılacaksa POST action eklemek gerekiyor.
+
+    //post action daha açmadım burayı view için yaptım -post action metotu içinde işlem yapmak icin dtoyu lazım 
+    //buradaki register, register page'inin viewi için var -register post action için ilerleyen zamanlarda register dtosuna göre hareket etmek lazm -tuncay
     public class AuthController : Controller
     {
-        [Route("register")]
-        public IActionResult Register()
-        {
-            return View();
-        }
-
-        [Route("login")]
+        //suanki layout'da loginin route'u bu ona uygun sekilde yaptım
+        //diger route'larıda ona entegre ettim. -tuncay
+        [Route("account/login")]
         public IActionResult Login()
         {
             return View();
         }
 
+        [Route("account/register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        
+
         //forgotpassword yerine forgot-password yapıldı--ödevde “arama motoru dostu” URL’ler önerilmiş.
-        [Route("login/forgot-password")]
+        [Route("account/forgot-password")]
         public IActionResult ForgotPassword()
         {
             return View();
@@ -28,12 +36,10 @@ namespace ECommerceWeb.MVC.Controllers
         //Aliye Not:
         //Logout action’ı view döndürüyor ama Ödevde “Kullanıcı Çıkış” için sadece action gerekli, view zorunlu değil.
         //Genellikle kullanıcıyı yönlendirir (RedirectToAction).
-        [Route("logout")] 
+        [Route("account/logout")]
         public IActionResult LogOut()
         {
-            return View();
-            // Çıkış işlemleri
-            //return RedirectToAction("Index", "Home"); ??
+            return RedirectToAction("Index", "Home");
         }
     }
 }
