6cd9f00f8d780a3e5e97a2dc5d3fb58a344d7f4e
AlyBlt <aly.bulut@gmail.com>
2025-11-19 23:52:52 +0300

    HomeController: Add routes for Home and Contact

--- ECommerce.Web.MVC/Controllers/HomeController.cs
+++ ECommerce.Web.MVC/Controllers/HomeController.cs
@@ -3,10 +3,14 @@ using ECommerce.Web.MVC.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerce.Web.MVC.Controllers
-{
+{             //AliyeNot: Routelar düzenlenecek.
+              //ProductDetail action’ý parametre almalý:Ödevde örnek route var: /product/{categoryName}-{title}-{id}/details
+              //Þu an action parametresiz, ileride Product ID veya kategori bilgisi ile çalýþacaksa eklenmeli
+              //yani-->>public IActionResult ProductDetail(int id, string categoryName, string title)
     public class HomeController : Controller
     {
-       
+
+        //[Route("home")]-->>böyle olabilir-->>URL’yi SEO ve okunabilirlik açýsýndan ayarlýyor.
         public IActionResult Index()
         {
             return View();
@@ -16,7 +20,7 @@ namespace ECommerce.Web.MVC.Controllers
         {
             return View(); 
         }
-        [Route("Contact")]
+        [Route("contact")] //Contact->contact yapýldý c küçük harfle--SEO dostu
         public IActionResult Contact()
         {
             return View();
@@ -27,6 +31,7 @@ namespace ECommerce.Web.MVC.Controllers
             return View();
         }
 
+        //[Route("product/{id}")] --->> public IActionResult ProductDetail(int id)
         public IActionResult ProductDetail()
         {
             return View();
