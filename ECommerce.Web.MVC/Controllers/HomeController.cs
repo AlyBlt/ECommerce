85c5730f7f089bb4d748bcc8d91b379fdd4af19d
Ömür Bayraktar <omurbayraktarr@gmail.com>
2025-11-19 20:22:47 +0300

    HomeController ve View entegrasyonu başlandı.
    wwwroot altındaki images klasörü img olarak yeniden adlandırıldı.
    HomeController action metotları ve ilgili View dosyaları oluşturuldu.
    Index, ProductDetail ve Contact sayfalarına HTML içerikleri eklendi.

--- ECommerce.Web.MVC/Controllers/HomeController.cs
+++ ECommerce.Web.MVC/Controllers/HomeController.cs
@@ -6,27 +6,30 @@ namespace ECommerce.Web.MVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+       
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult AboutUs()
+        {
+            return View(); 
+        }
+        [Route("Contact")]
+        public IActionResult Contact()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Listing()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
+        }
+
+        public IActionResult ProductDetail()
+        {
+            return View();
         }
     }
 }
