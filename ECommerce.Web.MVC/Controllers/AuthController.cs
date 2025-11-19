c701ce08933e3d0dc58c5707f5a8e10ff0135200
tombiks <tncybaktir@gmail.com>
2025-11-19 17:33:57 +0300

    auth controller created
    auth views created

---
+++ ECommerce.Web.MVC/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceWeb.MVC.Controllers
+{
+    public class AuthController : Controller
+    {
+        [Route("register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [Route("login/forgotpassword")]
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        [Route("logout")]
+        public IActionResult LogOut()
+        {
+            return View();
+        }
+    }
+}
