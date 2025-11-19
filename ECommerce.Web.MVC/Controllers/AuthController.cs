fc0bf52db8ab770f700c1798fee686df53f2177f
AlyBlt <aly.bulut@gmail.com>
2025-11-19 22:43:43 +0300

    AuthController branch merge: Register, Login, ForgotPassword, Logout actionları eklendi. Logout yönlendirmesi ve POST methodları ileride eklenebilir.

--- ECommerce.Web.MVC/Controllers/AuthController.cs
+++ ECommerce.Web.MVC/Controllers/AuthController.cs
@@ -2,6 +2,8 @@
 
 namespace ECommerceWeb.MVC.Controllers
 {
+    //Aliye Genel Not: Şu an sadece GET ile sayfayı döndürüyor.
+    //Ödevin bu kısmında form gönderimi yapılacaksa POST action eklemek gerekiyor.
     public class AuthController : Controller
     {
         [Route("register")]
@@ -16,16 +18,22 @@ namespace ECommerceWeb.MVC.Controllers
             return View();
         }
 
-        [Route("login/forgotpassword")]
+        //forgotpassword yerine forgot-password yapıldı--ödevde “arama motoru dostu” URL’ler önerilmiş.
+        [Route("login/forgot-password")]
         public IActionResult ForgotPassword()
         {
             return View();
         }
 
-        [Route("logout")]
+        //Aliye Not:
+        //Logout action’ı view döndürüyor ama Ödevde “Kullanıcı Çıkış” için sadece action gerekli, view zorunlu değil.
+        //Genellikle kullanıcıyı yönlendirir (RedirectToAction).
+        [Route("logout")] 
         public IActionResult LogOut()
         {
             return View();
+            // Çıkış işlemleri
+            //return RedirectToAction("Index", "Home"); ??
         }
     }
 }
