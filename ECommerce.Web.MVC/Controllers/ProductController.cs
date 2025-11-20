3c58cc90e199dbfeccd5678b0a11885514e66b7f
oguz-taskin <oguzz.taskin@gmail.com>
2025-11-21 00:33:10 +0300

    product model ve metodlarını yazııldı. getAllProducts view yazıldı

---
+++ ECommerce.Web.MVC/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using ECommerceWeb.MVC.Models;
+using ECommerceWeb.MVC.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceWeb.MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GetAllProducts()
+        {
+            var products = ProductService.GetAllProducts();
+
+            return View("dsfsdfdsf");
+        }
+    }
+}
---
+++ ECommerce.Web.MVC/Models/CategoryViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ECommerceWeb.MVC.Models
+{
+    public class CategoryViewModel
+    {
+        /// <summary>
+        /// Kategori Id
+        /// </summary>
+        public int CategoryId { get; set; }
+        /// <summary>
+        /// Kategori Adı
+        /// </summary>
+        public string CategoryName { get; set; }
+    }
+}
